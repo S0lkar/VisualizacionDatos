e3b9713f11f06118ca332c308c6f855ef8be5454
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2024-01-10 18:01:13 +0100

    Update main.py
    Added icon to pages nav

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ st.set_page_config(page_title="Active Satellites in Orbit Around Earth (2016)", 
 
 show_pages(
     [
-        Page("intro.py", "Sobre este trabajo"),
-        Page("bloque01.py", "Introducción al problema"),
-        Page("bloque02.py", "¿A quién pertenecen?"),
-        # Page("bloque03.py", "¿Qué propósito tienen?"),
-        Page("bloque04.py", "¿Qué órbitas siguen?"),
-        Page("bloque05.py", "¿Dónde se fabricaron?"),
-        # Page("bloque06.py", "¿Cómo se puede abordar el problema?"),
+        Page("intro.py", "Sobre este trabajo", "🌎"),
+        Page("bloque01.py", "Introducción al problema", "🌎"),
+        Page("bloque02.py", "¿A quién pertenecen?", "🌎"),
+        # Page("bloque03.py", "¿Qué propósito tienen?", "🌎"),
+        Page("bloque04.py", "¿Qué órbitas siguen?", "🌎"),
+        Page("bloque05.py", "¿Dónde se fabricaron?", "🌎"),
+        # Page("bloque06.py", "¿Cómo se puede abordar el problema?", "🌎"),
 
     ]
 )
