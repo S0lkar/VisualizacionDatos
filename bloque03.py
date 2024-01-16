ce9974fff5b49cb227ac88fdcaa7ada226b3c248
Danielgithub007 <150954650+Danielgithub007@users.noreply.github.com>
2024-01-16 11:14:15 +0100

    Add files via upload

--- bloque03.py
+++ bloque03.py
@@ -1,46 +1,47 @@
-from CommonTools import *
-# add to nav
-add_page_title(initial_sidebar_state="expanded", layout="wide")
-local_css("frontend.css")
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 18:05:42 2024
 
-def B3_01():
-    B3_0X = pd.read_csv(FILENAME)
-    # Eliminar filas con propósitos NaN
-    B3_0X = B3_0X.dropna(subset=['Purpose'])
+@author: Daniel007
+"""
 
-    # Dividir la columna 'Purpose' en múltiples columnas para cada propósito
-    B3_0X['Purposes'] = B3_0X['Purpose'].apply(lambda x: str(x).split(','))
-    B3_0X = B3_0X.explode('Purposes')
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-    # Crear un gráfico interactivo con Plotly
-    fig = px.bar(B3_0X, x='Country of Operator/Owner', color='Purposes', text='Official Name of Satellite',
-                title='Number of Satellites by Purpose for Each Country of Operator/Owner',
-                labels={'Country of Operator/Owner': 'Country of Operator/Owner', 'Purposes': 'Purpose'},
-                category_orders={'Purposes': sorted(B3_0X['Purposes'].unique())},
-                height=500)
+import warnings
+warnings.filterwarnings("ignore")
 
-    # Personalizar el diseño
-    fig.update_layout(xaxis_title='Country of Operator/Owner', yaxis_title='Number of Satellites',
-                    legend_title_text='Purpose', barmode='stack')
+database = pd.read_csv('database.csv')
 
-    # Añadir interactividad al hacer clic en un país de operador/propietario
-    fig.update_traces(selector=dict(type='bar'), hoverinfo='y+name')
+# Crear un DataFrame de pandas
+B3_0X = pd.DataFrame(database)
 
-    # Mostrar el gráfico interactivo en el navegador
-    return fig
+# Eliminar filas con propósitos NaN
+B3_0X = B3_0X.dropna(subset=['Purpose'])
 
+# Dividir la columna 'Purpose' en múltiples columnas para cada propósito
+B3_0X['Purposes'] = B3_0X['Purpose'].apply(lambda x: str(x).split(','))
+B3_0X = B3_0X.explode('Purposes')
 
-def B3_Frontend():
-    audio_file = open("audio/intro.mp3", "rb")
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format="audio/mpeg")
-    
-    st.subheader("| Título")
-    st.write("""
-             Lorem ipsum noseque nosecuantos.
-             """)
-    PositionImage(B3_01())
-    
-    
-    
-B3_Frontend()
+# Filtrar países con más de 20 satélites
+countries_with_more_than_20_satellites = B3_0X['Country of Operator/Owner'].value_counts()[B3_0X['Country of Operator/Owner'].value_counts() > 20].index
+B3_0X_filtered = B3_0X[B3_0X['Country of Operator/Owner'].isin(countries_with_more_than_20_satellites)]
+
+# Crear un gráfico interactivo con Plotly
+fig = px.bar(B3_0X_filtered, x='Country of Operator/Owner', color='Purposes', text='Official Name of Satellite',
+             title='Number of Satellites by Purpose for Each Country of Operator/Owner (Countries with >20 Satellites)',
+             labels={'Country of Operator/Owner': 'Country of Operator/Owner', 'Purposes': 'Purpose'},
+             category_orders={'Purposes': sorted(B3_0X_filtered['Purposes'].unique())},
+             height=500)
+
+# Personalizar el diseño
+fig.update_layout(xaxis_title='Country of Operator/Owner', yaxis_title='Number of Satellites',
+                  legend_title_text='Purpose', barmode='stack')
+
+# Añadir interactividad al hacer clic en un país de operador/propietario
+fig.update_traces(selector=dict(type='bar'), hoverinfo='y+name')
+
+# Mostrar el gráfico interactivo en el navegador
+fig.show()
+B3_0X=fig
